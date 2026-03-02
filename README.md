4b07a61c51b30466801a5d957432c38b5f334050
Mehrunnisa <syedamehrunnisa21feb@gmail.com>
2026-03-02 20:14:36 +0500

    Version 2: Added Grading System

---
+++ README.md
@@ -0,0 +1,2 @@
+Commit 2:
+This is the version 2 of app in which grading system is also added.
--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main() {
     int marks1, marks2, marks3;
     int total;
     float average;
+    char grade;
 
     cout << "Enter student name: ";
     cin >> name;
@@ -16,10 +17,23 @@ int main() {
     total = marks1 + marks2 + marks3;
     average = total / 3.0;
 
+    // Grade logic added
+    if (average >= 80)
+        grade = 'A';
+    else if (average >= 70)
+        grade = 'B';
+    else if (average >= 60)
+        grade = 'C';
+    else if (average >= 50)
+        grade = 'D';
+    else
+        grade = 'F';
+
     cout << "\n--- Result ---\n";
     cout << "Name: " << name << endl;
     cout << "Total: " << total << endl;
     cout << "Average: " << average << endl;
+    cout << "Grade: " << grade << endl;
 
     return 0;
 }
