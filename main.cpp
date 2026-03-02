5157661132a998e45aedebbafdfbe847282fef0d
Mehrunnisa <syedamehrunnisa21feb@gmail.com>
2026-03-02 20:16:46 +0500

    Version 3: Refactored Program Using OOP Structure

--- main.cpp
+++ main.cpp
@@ -1,39 +1,53 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+class Student {
+private:
     string name;
     int marks1, marks2, marks3;
     int total;
     float average;
     char grade;
 
-    cout << "Enter student name: ";
-    cin >> name;
+public:
+    void input() {
+        cout << "Enter student name: ";
+        cin >> name;
 
-    cout << "Enter marks for 3 subjects:\n";
-    cin >> marks1 >> marks2 >> marks3;
+        cout << "Enter marks for 3 subjects:\n";
+        cin >> marks1 >> marks2 >> marks3;
+    }
 
-    total = marks1 + marks2 + marks3;
-    average = total / 3.0;
+    void calculate() {
+        total = marks1 + marks2 + marks3;
+        average = total / 3.0;
 
-    // Grade logic added
-    if (average >= 80)
-        grade = 'A';
-    else if (average >= 70)
-        grade = 'B';
-    else if (average >= 60)
-        grade = 'C';
-    else if (average >= 50)
-        grade = 'D';
-    else
-        grade = 'F';
+        if (average >= 80)
+            grade = 'A';
+        else if (average >= 70)
+            grade = 'B';
+        else if (average >= 60)
+            grade = 'C';
+        else if (average >= 50)
+            grade = 'D';
+        else
+            grade = 'F';
+    }
 
-    cout << "\n--- Result ---\n";
-    cout << "Name: " << name << endl;
-    cout << "Total: " << total << endl;
-    cout << "Average: " << average << endl;
-    cout << "Grade: " << grade << endl;
+    void display() {
+        cout << "\n--- Result ---\n";
+        cout << "Name: " << name << endl;
+        cout << "Total: " << total << endl;
+        cout << "Average: " << average << endl;
+        cout << "Grade: " << grade << endl;
+    }
+};
+
+int main() {
+    Student s;
+    s.input();
+    s.calculate();
+    s.display();
 
     return 0;
 }
